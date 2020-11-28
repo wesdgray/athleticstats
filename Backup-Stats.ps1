56ab09d8f7c1a5ba975ab6830d0624c6c800facd
wesdgray <wesgray@gmail.com>
2020-11-28 13:56:45 -0800

    Create Backup-Stats.ps1
    Script to back up stats computer

---
+++ Backup-Stats.ps1
@@ -0,0 +1,45 @@
+Function Backup-Stats
+{
+    param(
+        $source         = 'V:\2020',
+        $destination    = 'S:\Football\Backup',
+        $backupInterval = 60
+    )
+    
+    $ProgressBarColorSuccess = 'DarkCyan'
+    $ProgressBarColorError = 'Red'
+    
+    $isPathGood = $(Test-Path -Path $source) -or $(Test-Path -Path $destination)
+    
+    if ( -not $isPathGood )
+    {
+        Write-Host "Check source or destination path is reachable! `r`n Source: $source `r`n Destination: $destination"
+        return
+    }
+    
+    while($true) 
+    {
+        robocopy $source $destination * /mir | Write-Output
+        
+        for( $i=0; $i -lt $backupInterval; $i++)
+        {
+            if ($LASTEXITCODE -ge 8) 
+            {
+                $host.privatedata.ProgressBackgroundColor = $ProgressBarColorFailure
+                $status = 'BAD'
+            }
+            else
+            {
+                $host.privatedata.ProgressBackgroundColor = $ProgressBarColorSuccess
+                $status = 'GOOD'
+            }
+            
+            Write-Progress -Activity "Robocopy from $source to $destination"`
+                           -Status "Status: $status; Time until next backup..."`
+                           -PercentComplete $( 100 * ($i / $backupInterval) )`
+                           -SecondsRemaining $( $backupInterval - $i )
+            
+            Start-Sleep -Seconds 1
+        }
+    }
+}
